18c45449b4d60ca60f5e8f7151258fda65c374c5
Joel Rothschild <joel.rothschild@jacksonriver.com>
2024-02-01 16:07:30 +0200

    Refactored HealthCheckLinks script to upgrade performance.

--- maintenance/HealthCheckLinks.php
+++ maintenance/HealthCheckLinks.php
@@ -24,11 +24,19 @@ class HealthCheckLinks extends KZBrokenLinksMaintenance {
 		parent::__construct();
 		$this->addDescription( "CLI utility to poll the health of external links recorded in Google Sheets" );
 		$this->addOption( 'runtime',
-			'Maximum number of seconds to execute before exiting (default 300)',
+			'Maximum number of seconds to execute before exiting (default 60)',
 			false, true
 		);
 		$this->addOption( 'maxlinks',
 			'Maximum number of links to process before exiting (default unlimited)',
+			false, true
+		);
+		$this->addOption( 'batchsize',
+			'Maximum number of link status rows per callout to the Google Sheets batch update API (default 20)',
+			false, true
+		);
+		$this->addOption( 'querysize',
+			'Maximum number of rows to query per callout to the Google Sheets get API (default 1000)',
 			false, true
 		);
 
@@ -42,8 +50,12 @@ class HealthCheckLinks extends KZBrokenLinksMaintenance {
 	 * @inheritDoc
 	 */
 	public function execute() {
+		// Update batch size and get query size
+		$query_size = intval( $this->getOption( 'querysize', 1000 ) );
+		$batch_size = intval( $this->getOption( 'batchsize', 20 ) );
+
 		// Time limit
-		$max_secs = $this->getOption( 'runtime', 300 );
+		$max_secs = $this->getOption( 'runtime', 60 );
 		$end_time = time() + $max_secs;
 
 		// Optionally also limit total number of links/rows
@@ -67,68 +79,106 @@ class HealthCheckLinks extends KZBrokenLinksMaintenance {
 
 		// Check links health
 		$domains_checked = [];
-		$row_num = 2;
+		$end_reached = false;
 		$today_day = date( 'j' );
 		$today_month = date( 'n' );
 		$today_year = date( 'Y' );
+		$first_nonskip_row = 2;
 		while ( time() < $end_time && ( $max_links === 0 || $processed_count < $max_links ) ) {
-			// Query the next row in NEXT_CHECK sheet
-			$row = $service->spreadsheets_values->get( $spreadsheetId, "NEXT_CHECK!A{$row_num}:F{$row_num}" );
-			if ( empty( $row ) || empty( $row->getValues()[0] ) || empty( $row->getValues()[0][1] ) ) {
-				$this->output( "Row number {$row_num} in NEXT_CHECK sheet is empty. Exiting.\n" );
+			// Query NEXT_CHECK rows, run checks, and amass updates until we either reach maximum
+			// batch size or run out of rows to check.
+			$status_updates = [];
+			$start_row = $first_nonskip_row;
+			while ( count( $status_updates ) < $batch_size && time() < $end_time ) {
+				// Query enough rows in NEXT_CHECK to fill the update batch (if we don't skip any)
+				$end_row = $start_row + $query_size;
+				$rows = $service->spreadsheets_values->get( $spreadsheetId, "NEXT_CHECK!A{$start_row}:F{$end_row}" );
+				if ( empty( $rows ) || empty( $rows->getValues()[0] ) || $rows->getValues()[0][0] == '#N/A' ) {
+					$this->output( "Row number {$start_row} in NEXT_CHECK sheet is empty. Preparing to exit.\n" );
+					$end_reached = true;
+					break;
+				}
+				// Loop through the returned rows and perform health checks.
+				for (
+					$row_i = 0;
+					!empty( $rows->getValues()[$row_i] ) && !empty( $rows->getValues()[$row_i][1] )
+						&& count( $status_updates ) < $batch_size;
+					$row_i++, $start_row++
+				) {
+					// Parse URL.
+					$url = $rows->getValues()[$row_i][1];
+					preg_match( "|([a-zA-Z]+)\\:\\/*([^\\/]+)\\/*(.*)|", $url, $url_components );
+					$protocol = $url_components[1];
+					$domain = $url_components[2];
+
+					// Remove anchor portion from end of URL.
+					if ( strpos( $url, '#' ) !== false ) {
+						$url = substr( $url, 0, strpos( $url, '#' ) );
+					}
+
+					// Don't call out twice to the same domain within a single run.
+					if ( in_array( $domain, $domains_checked ) ) {
+						// Skip this row.
+//						$this->output( "Skipping additional link to domain $domain: $url\n" );
+						if ( $first_nonskip_row == $start_row ) {
+							// There are no non-skip rows before this one, so future queries can skip all rows up to
+							// and including this one.
+							$first_nonskip_row++;
+						}
+						continue;
+					} elseif ( in_array( $protocol, $excluded_protocols ) ) {
+						// Don't call out to an excluded protocol.
+						$this->output( "Skipping URL with an excluded protocol: $url\n" );
+						if ( $first_nonskip_row == $start_row ) {
+							// There are no non-skip rows before this one, so future queries can skip all rows up to
+							// and including this one.
+							$first_nonskip_row++;
+						}
+						continue;
+					} else {
+						// Add the domain to those we'll skip in any subsequent row during this run.
+						$domains_checked[] = $domain;
+					}
+
+					// Call out to URL and check response status
+					$last_status = $rows->getValues()[$row_i][5] ?? 0;
+					$result = $this->calloutToUrl( $url, $protocol, $last_status >= 300 );
+					$redirectUrl = ( strtolower( $result['finalUrl'] ) != $url ) ? $result['finalUrl'] : '';
+
+					// Add status update to batch.
+					$update_row = $rows->getValues()[$row_i][0];
+					$updated_values = [ [
+						"=DATE({$today_year},{$today_month},{$today_day})",
+						$result['code'],
+						$redirectUrl,
+						$result['error'] ?? '',
+					] ];
+					$status_updates[] = new \Google\Service\Sheets\ValueRange( [
+						'range' => "LINKS_STATUS!E{$update_row}:H{$update_row}",
+						'values' => $updated_values,
+					] );
+
+					$this->output( "$url status: {$result['code']}\n" );
+				}
+				$this->maintainRateLimit();
+			}
+
+			// Batch update LINKS_STATUS sheet with the collected health check data.
+			$batch_rows = count( $status_updates );
+			$this->output( "Batch updating {$batch_rows} rows in LINKS_STATUS...\n" );
+			$result = $service->spreadsheets_values->batchUpdate(
+				$spreadsheetId,
+				new \Google\Service\Sheets\BatchUpdateValuesRequest( [
+					'valueInputOption' => 'USER_ENTERED',
+					'data' => $status_updates,
+				] )
+			);
+			if ( $end_reached ) {
+				// No more rows left to process, so we're done.
 				return;
 			}
 			$this->maintainRateLimit();
-
-			// Parse URL.
-			$url = $row->getValues()[0][1];
-			preg_match( "|([a-zA-Z]+)\\:\\/*([^\\/]+)\\/*(.*)|", $url, $url_components );
-			$protocol = $url_components[1];
-			$domain = $url_components[2];
-
-			// Remove anchor portion from end of URL.
-			if ( strpos( $url, '#' ) !== false ) {
-				$url = substr( $url, 0, strpos( $url, '#' ) );
-			}
-
-			// Don't call out twice to the same domain within a single run.
-			if ( in_array( $domain, $domains_checked ) ) {
-				// Skip this row.
-				$this->output( "Skipping additional link to domain $domain: $url\n" );
-				$row_num++;
-				continue;
-			} elseif ( in_array( $protocol, $excluded_protocols ) ) {
-				// Don't call out to an excluded protocol.
-				$this->output( "Skipping URL with an excluded protocol: $url\n" );
-				$row_num++;
-				continue;
-			} else {
-				// Add the domain to those we'll skip in any subsequent row during this run.
-				$domains_checked[] = $domain;
-			}
-
-			// Call out to URL and check response status
-			$last_status = $row->getValues()[0][5] ?? 0;
-			$result = $this->calloutToUrl( $url, $protocol, $last_status >= 300 );
-			$redirectUrl = ( strtolower( $result['finalUrl'] ) != $url ) ? $result['finalUrl'] : '';
-
-			// Update row in NEXT_CHECK sheet with response status and today's date.
-			$update_row = $row->getValues()[0][0];
-			$updated_values = [ [
-				"=DATE({$today_year},{$today_month},{$today_day})",
-				$result['code'],
-				$redirectUrl,
-				$result['error'] ?? '',
-			] ];
-			$valueRange = new \Google\Service\Sheets\ValueRange();
-			$valueRange->setValues( $updated_values );
-			$firstCell = "LINKS_STATUS!E{$update_row}";
-			$service->spreadsheets_values->update(
-				$spreadsheetId, $firstCell, $valueRange, [ 'valueInputOption' => 'USER_ENTERED' ]
-			);
-			$this->maintainRateLimit();
-
-			$processed_count++;
+			$processed_count += $batch_rows;
 		}
 	}
 
